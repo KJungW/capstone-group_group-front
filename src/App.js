d70d10c7f90f0ccd3c56512ef1071fe0cedf3246
SeungMin990822 <dtadta1234@naver.com>
2024-03-23 05:29:48 +0900

    ver1 commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import 'styles/App.module.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from 'components/Header';
-import Nav from 'components/Nav';
-import Board from 'components/Board';
-import Applications from 'components/Applications';
-import Recruitments from 'components/Recruitments';
-import Recruit from 'components/Recruit';
-import RecruitDetail from 'components/RecruitDetail';
+import React from "react";
+import "styles/App.module.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "components/Header";
+import Nav from "components/Nav";
+import Board from "components/Board";
+import Applications from "components/Applications";
+import Recruitments from "components/Recruitments";
+import Recruit from "components/Recruit";
+import RecruitDetail from "components/RecruitDetail";
+import Request from "components/join_request";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path="/recruitments" element={<Recruitments />} />
           <Route path="/recruit" element={<Recruit />} />
           <Route path="/recruitdetail" element={<RecruitDetail />} />
+          <Route path="/request" element={<Request />} />
         </Routes>
       </div>
     </Router>
--- src/components/join_request.js
+++ src/components/join_request.js
@@ -1,11 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "styles/join_request.css";
-import Item from "./Item";
 
 const mockData = [
-  // 일단 임시로 데이터 넣었음
-
   {
     id: 1,
     condition: "github 주소",
@@ -24,6 +21,13 @@ const mockData = [
     file: false,
     content: "",
   },
+
+  {
+    id: 4,
+    condition: "간단한 코드",
+    file: true,
+    content: "",
+  },
 ];
 
 const Request = () => {
@@ -33,69 +37,49 @@ const Request = () => {
   const [data, setData] = useState(mockData);
   const navigate = useNavigate();
 
-  const handleMenuClick = (menu) => {
-    // 사용자가 확인을 눌렀을 때만 메뉴를 변경합니다.
-    const userConfirmation = window.confirm(
-      "작성중인 페이지를 벗어나시겠습니까?"
-    );
-    if (userConfirmation) {
-      setSelectedMenu(menu);
-      setBoardName(getMenuName(menu));
-      setShowWriteButton(menu === "board");
-    }
-  };
-
-  const getMenuName = (menu) => {
-    let confirmationMessage = "";
-
-    switch (menu) {
-      case "board":
-        confirmationMessage = "게시판";
-        break;
-      case "applications":
-        confirmationMessage = "작성글 목록";
-        break;
-      case "recruitments":
-        confirmationMessage = "신청 목록";
-        break;
-      default:
-        return "";
+  const renderInputLine = (dataItem) => {
+    if (dataItem.file) {
+      return (
+        <div className="inputline">
+          <ul className="list">
+            <li>
+              <button className="upload-button">파일 업로드</button>
+            </li>
+            <li className="content-list">
+              <input type="text" className="input-content" />
+            </li>
+          </ul>
+        </div>
+      );
+    } else {
+      return (
+        <div className="inputline">
+          <input type="text" className="input-content" />
+        </div>
+      );
     }
   };
 
   return (
     <div className="scroll-container">
       <div className="board-map">
-        <div className="menu-bar">
-          <button
-            onClick={() => handleMenuClick("board")}
-            className={selectedMenu === "board" ? "active" : ""}
-          >
-            게시판
-          </button>
-          <button
-            onClick={() => handleMenuClick("applications")}
-            className={selectedMenu === "applications" ? "active" : ""}
-          >
-            작성글 목록
-          </button>
-          <button
-            onClick={() => handleMenuClick("recruitments")}
-            className={selectedMenu === "recruitments" ? "active" : ""}
-          >
-            신청 목록
-          </button>
-        </div>
         <div className="board-content">
           <div className="wrap1">
             <div className="board-name">신청하기</div>
           </div>
           <div className="contents-box">
             {mockData.map((dataItem) => (
-              <Item key={dataItem.id} data={dataItem} />
+              <div key={dataItem.id} className="Item">
+                <div className="requirement">
+                  <div>
+                    참여요건{dataItem.id}:{dataItem.condition}
+                  </div>
+                </div>
+                {renderInputLine(dataItem)}
+              </div>
             ))}
           </div>
-          <button className="save">저장하기</button>
+          <button className="save-button">저장하기</button>
         </div>
       </div>
     </div>
