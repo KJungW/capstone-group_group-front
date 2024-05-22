4b9211f20b3b87e46aaa8e60c0791a802602adc5
KJungW <sagakkb0707@naver.com>
2024-05-22 21:16:24 +0900

    Feat(Signup): 고침 회원가입 모달창 버그
    회원가입 관련 문구수정
    이메일 재전송 모달창의 안내문구 중앙정렬
    중복 회원가입 버그 수정
    닉네임 검증메세지 버그 수정

--- src/components/signup/SignUpFormModal.js
+++ src/components/signup/SignUpFormModal.js
@@ -22,10 +22,14 @@ const SignUpFormModal = ({moveNextModal}) => {
   const [pwErrorMsg, setPwErrorMsg] = useState("");
   const [pwCheckErrorMsg, setPwCheckErrorMsg] = useState("");
 
+  const [isInSignProgress, setIsInSignProgress] = useState(false);
+
   // input 태그 입력 처리메소드
   const onChangeInputEmail = (e) => {setInputEmail(e.target.value);};
   const onChangeInputNickName = (e) => {
-    setInputNickName(e.target.value);
+    const inputString = e.target.value;
+    if(inputString[inputString.length - 1] === " ") return;
+    setInputNickName(inputString);
     setNickNameAvailable(false);
   };
   const onChangeInputPw = (e) => {setInputPw(e.target.value);}
@@ -37,28 +41,29 @@ const SignUpFormModal = ({moveNextModal}) => {
     // 이메일 검증
     let emailError;
     if(!inputEmail || inputEmail.trim() === "") {
-      emailError="이메일을 입력해주세요";
+      emailError="이메일을 입력해주세요.";
     } else if (!emailRegex.test(inputEmail)) {
-      emailError="유효한 이메일을 입력해주세요";
+      emailError="유효한 이메일을 입력해주세요.";
     } else {
       emailError="";
     }
 
     // 닉네임 검증
     let nickNameError;
-    if(!nickNameAvailable) {
-      nickNameError="닉네임이 사용가능한지 확인해주세요";
+    if(!inputNickName || inputNickName.trim() === "") {
+      nickNameError="닉네임을 입력해주세요.";
     }
-    else if(!inputNickName || inputNickName.trim() === "") {
-      nickNameError="닉네임을 입력해주세요";
-    } else {
+    else if(!nickNameAvailable) {
+      nickNameError="닉네임 중복 확인을 해주세요.";
+    }
+    else {
       nickNameError="";
     }
 
     // 비밀번호 검증
     let pwError
     if(!inputPw || inputPw.trim() === "") {
-      pwError="비밀번호를 입력해주세요";
+      pwError="비밀번호를 입력해주세요.";
     } else if (!passwordRegex.test(inputPw)){
       pwError="비밀번호는 8자 이상 15자 이내의 영문, 숫자, 특수문자를 포함해야 합니다.";
     } else {
@@ -68,7 +73,7 @@ const SignUpFormModal = ({moveNextModal}) => {
     // 비밀번호 체크 검증
     let pwCheckError
     if(!inputPwCheck || inputPwCheck.trim() === "") {
-      pwCheckError="비밀번호를 한번더 입력해주세요";
+      pwCheckError="비밀번호를 한 번 더 입력해주세요.";
     } else if (inputPwCheck!==inputPw){
       pwCheckError="비밀번호가 일치하지 않습니다.";
     } else {
@@ -86,15 +91,27 @@ const SignUpFormModal = ({moveNextModal}) => {
   }
 
   // 회원가입 수행
-  const signUp = () => {
-    console.log("회원가입 수행");
-    reqeustSignUpApi(inputEmail, inputNickName, inputPw);
-    moveNextModal(inputEmail, inputNickName, inputPw);
+  const signUp = async () => {
+    try {
+      console.log("SignUpFormModal : 회원가입 인증이메일 요청 시작");
+      setIsInSignProgress(true);
+      await reqeustSignUpApi(inputEmail, inputNickName, inputPw);
+      console.log("SignUpFormModal : 회원가입 인증이메일 요청 성공");
+      moveNextModal(inputEmail, inputNickName, inputPw);
+    } catch(err) {
+      console.log("SignUpFormModal : 회원가입 인증이메일 요청 실패");
+      handleApiReqeustError({
+        err:err,
+        handleBadInput: () => {alert("이미 등록된 이메일입니다.");}
+      })
+    } finally {
+      setIsInSignProgress(false);
+    }
   }
 
   // 넥네임 확인 수행
   const checkNickNameAvailable = async () => {
-    if(!inputNickName) {
+    if(!inputNickName || inputNickName.trim() === "") {
       alert("닉네임을 먼저 입력해주세요");
       return;
     }
@@ -103,24 +120,23 @@ const SignUpFormModal = ({moveNextModal}) => {
       console.log("SignUpFormModal : 닉네임 이용가능 여부 조회 시작");
       await requestFindNickNameAvailabilityApi(inputNickName);
       console.log("SignUpFormModal : 닉네임 이용가능 여부 조회 성공");
-      alert("이용가능한 닉네임입니다.");
+      alert("사용 가능한 닉네임입니다.");
       setNickNameAvailable(true);
+      setNickNameErrorMsg("");
     } catch (err) {
       console.log("SignUpFormModal : 닉네임 이용가능 여부 조회 실패");
       handleApiReqeustError({
         err:err,
-        handleBadInput: () => {
-          alert("이미 사용되고 있는 닉네임입니다.")
-        }
+        handleBadInput: () => {alert("이미 사용 중인 닉네임입니다.");}
       })
     }
   }
 
   // 회원가입 버튼클릭 메서드
-  const clickSingUpBtn = () => {
-    let validateResult = validateInput()
+  const clickSingUpBtn = async () => {
+    let validateResult = validateInput();
     if(validateResult.isValid) {
-      signUp();
+      await signUp();
     } 
     setEmailErrorMsg(validateResult.emailErrorMsg);
     setNickNameErrorMsg(validateResult.nickNameErrorMsg);
@@ -147,7 +163,7 @@ const SignUpFormModal = ({moveNextModal}) => {
           <div className={styles.labelBox}><span>사용할 닉네임</span></div>
           <div className={styles.nickNameInputBox}>
             <input type="text" className={styles.nickNameInput} onChange={onChangeInputNickName} value={inputNickName} maxLength={20}/>
-            <button className={styles.nickNameCheckBtn} onClick={clickNickNameCheckBtn} disabled={nickNameAvailable}>닉네임 확인</button>
+            <button className={styles.nickNameCheckBtn} onClick={clickNickNameCheckBtn} disabled={nickNameAvailable}>중복확인</button>
           </div>
           <div className={styles.errorMsg}>{nickNameErrorMsg}</div>
         </div>
@@ -161,7 +177,7 @@ const SignUpFormModal = ({moveNextModal}) => {
           <input type="password" className={styles.textInput} onChange={onChangeInputPwCheck} value={inputPwCheck} maxLength={15}/>
           <div className={styles.errorMsg}>{pwCheckErrorMsg}</div>
         </div>
-        <button className={styles.signUpButton} onClick={clickSingUpBtn}>회원가입</button>
+        <button className={styles.signUpButton} onClick={clickSingUpBtn} disabled={isInSignProgress}>{isInSignProgress?'진행중':'회원가입'}</button>
       </div>
     </div>
   );
